d72115c598ddac0dc69f17ee8e3715073255f75a
Kavin Inthirakot <kainthirakot@gmail.com>
2020-11-12 20:03:18 -0500

    add new function broke up function note: need debuggin :(

--- Stacks.cpp
+++ Stacks.cpp
@@ -7,40 +7,73 @@ using namespace std;
 double accountBalance = 0.00;
 
 //functions
-void deposit(double a){  
-
+void exitMessage() {
+    cout<<"Bye! Thank's for visiting Kav's Bank Have a nice day!\n";
+    
 }
-void withdraw (){
+void menu() {
 
-}
-void checkBalance() {
-    cout << "Your Current Balance: " << fixed << setprecision(2) << accountBalance << "\n";
-}
-
-//driver code
-int main() {
+    double moneyIn;
+    double moneyOut;
     int number;
-    double money;
 
-    cout<<"--------------------------------------------------\n";
-    cout<<"Welcome to Kav's Bank How can I help you today? \n";
-    cout<<"--------------------------------------------------\n";
     cout<<"Enter 1: Deposit \n";
     cout<<"Enter 2: Withdraw \n";
     cout<<"Enter 3: Check Balance \n";
+    cout<<"Enter 4: Exit \n";
     cin >> number;
 
     cout<<"\n";
+}
+void deposit(double money){
+
+    accountBalance = accountBalance - money;
+    cout<< "Transaction complete! \n";
+    menu();
+
+}
+void withdraw (double money){
+    if (accountBalance < money) {
+        cout << "Transaction failed. Insufficent fund detected, try again later \n";
+    } else {
+        accountBalance = accountBalance - money;
+        cout<< "Transaction complete! \n";
+    }
+    menu();
+}
+void checkBalance() {
+    cout << "Your Current Balance: " << fixed << setprecision(2) << "$" << accountBalance << "\n";
+}
+int options(int number) {
+    double moneyIn;
+    double moneyOut;
 
     if(number == 1) {
         cout<<"How much would you like to deposit? \n";
-        cin >> money;
-        
-        deposit(money);
+        cin >> moneyIn;
+        deposit(moneyIn);
+    }
+    if(number == 2){
+        cout<<"How much would you like to withdraw? \n";
+        cin >> moneyOut;
+        withdraw(moneyOut);
     }
     if(number == 3) {
         checkBalance();
     }
+    if(number == 4) {
+        exitMessage();
+    }
+}
+
+
+//driver code
+int main() {
+
+    cout<<"--------------------------------------------------\n";
+    cout<<"Welcome to Kav's Bank How can I help you today? \n";
+    cout<<"--------------------------------------------------\n";
+    menu();
 
   return 0;
 }
