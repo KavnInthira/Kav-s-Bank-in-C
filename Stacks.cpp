271b94c3d0f2cdb94dd54693c6543d696399bdd2
Kavin Inthirakot <kainthirakot@gmail.com>
2020-11-12 21:22:23 -0500

    add looping to message need to fix why infinite loop after multiple runs

--- Stacks.cpp
+++ Stacks.cpp
@@ -7,40 +7,67 @@ using namespace std;
 double accountBalance = 0.00;
 
 //functions
-void deposit(double a){  
+void deposit(double money){  
+    int number;
+    accountBalance = accountBalance + money;
+    cout << "Transaction Complete! \n\n";
 
 }
-void withdraw (){
-
+void withdraw (double money){
+    if(accountBalance < money){
+        cout << "Insufficent Funds! Please try again later.";
+    } else {
+        accountBalance = accountBalance - money;
+        cout <<"Transaction Complete! \n\n";
+    }
 }
 void checkBalance() {
-    cout << "Your Current Balance: " << fixed << setprecision(2) << accountBalance << "\n";
+    cout << "Your Current Balance: " << fixed << setprecision(2) << "$"<< accountBalance << "\n\n";
+}
+void exitMessage(){
+    cout <<"Goodbye, Have a nice day! \n\n";
 }
 
 //driver code
 int main() {
     int number;
-    double money;
+    double moneyIn;
+    double moneyOut;
+    
 
     cout<<"--------------------------------------------------\n";
     cout<<"Welcome to Kav's Bank How can I help you today? \n";
-    cout<<"--------------------------------------------------\n";
-    cout<<"Enter 1: Deposit \n";
-    cout<<"Enter 2: Withdraw \n";
-    cout<<"Enter 3: Check Balance \n";
-    cin >> number;
+    
+    int i = 0;
+    while(i < 1){
+        cout<<"----------------------------------------------\n";
+        cout <<"Please enter one of the options.\n";
+        cout<<"Enter 1: Deposit \n";
+        cout<<"Enter 2: Withdraw \n";
+        cout<<"Enter 3: Check Balance \n";
+        cout<<"Enter 4: Exit \n";
+        cout<<"----------------------------------------------\n";
+        cin >> number;
+        cout<<"\n";
 
-    cout<<"\n";
-
-    if(number == 1) {
-        cout<<"How much would you like to deposit? \n";
-        cin >> money;
-        
-        deposit(money);
+        if(number == 1) {
+            cout<<"How much would you like to deposit? \n";
+            cin >> moneyIn;
+            deposit(moneyIn);
+        }
+        if(number == 2){
+            cout<<"How much would you like to withdraw? \n";
+            cin>>moneyOut;
+            withdraw(moneyOut);
+        }
+        if(number == 3) {
+            checkBalance();
+        }
+        if(number == 4) {
+            exitMessage();
+            i = i + 1;
+        }
     }
-    if(number == 3) {
-        checkBalance();
-    }
-
+    
   return 0;
 }
